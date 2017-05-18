fba2be30d1b3853c44838a8dacd8b27ae07fbe06
eric-cheng <magnadoodle@gmail.com>
2017-05-18 16:21:01 -0400

    just adding notes into the code

--- generate_figure.py
+++ generate_figure.py
@@ -3,6 +3,8 @@ import matplotlib as mpl
 mpl.use('Agg')
 import seaborn as sns
 
+# this is a test
+
 df = sns.load_dataset('tips')
 seaborn_plot = sns.pairplot(df)
 seaborn_plot.savefig('pairplot.png')
